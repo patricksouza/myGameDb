6b87fe61298fcfb92b75ecf9bbd67a629af0a02f
Patrick Miranda <patrickrsmiranda@gmail.com>
2021-05-19 19:24:30 -0400

    [001] First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Inserted modules
+import { FormsModule } from '@angular/forms';
+//import { HttpClientModules } from '@angular/common/http'
+import { GaugeModule } from 'angular-gauge';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
