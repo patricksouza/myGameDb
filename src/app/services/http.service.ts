c533b0f6ac85dc202c1711aecb61b6ca4302714a
Patrick Miranda <patrickrsmiranda@gmail.com>
2021-05-23 19:27:44 -0400

    [010] Fixing url from api

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment as env } from 'environments/environment';
+import { APIResponse, Data } from 'app/models/data.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getValuesList(
+    ordering: string,
+    search?: string
+  ): Observable<APIResponse<Data>> {
+    let params = new HttpParams().set('ordering', ordering);
+    //if a search was requested do this
+    if (search) {
+      params = new HttpParams().set('ordering', ordering).set('search', search);
+    }
+
+    return this.http.get<APIResponse<Data>>(`${env.BASE_URL}/games`, {
+      params: params,
+    });
+  }
 }
