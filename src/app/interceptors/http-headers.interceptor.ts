e5da8e8c0f3c89b2b1ce6560abe41402ca0cb621
Patrick Miranda <patrickrsmiranda@gmail.com>
2021-05-23 19:33:20 -0400

    [014] Working on interceptors

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -18,11 +18,11 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     request = request.clone({
       setHeaders: {
-        'x-rapidapi-key': 'eb4786c419mshe3372ce6646c58ep19f044jsn4878fa0ed543',
-        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+        'x-rapidapi-key': '',
+        'x-rapidapi-host': '',
       },
       setParams: {
-        key: 'e40e743af2c94b0c916a8aa618fb4473',
+        key: '',
       },
     });
     return next.handle(request);
