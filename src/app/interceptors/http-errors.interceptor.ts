b602c89fb4b0730684a26e87e3065d3c9c0d5107
Patrick Miranda <patrickrsmiranda@gmail.com>
2021-05-23 19:33:20 -0400

    [014] Working on interceptors

---
+++ src/app/interceptors/http-errors.interceptor.ts
@@ -0,0 +1,28 @@
+//Error handling
+
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError as observableThrowError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class HttpErrorsInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error) => {
+        console.log(error);
+        return observableThrowError(error);
+      })
+    );
+  }
+}
---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,30 @@
+//Receive data from api response
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    request = request.clone({
+      setHeaders: {
+        'x-rapidapi-key': 'eb4786c419mshe3372ce6646c58ep19f044jsn4878fa0ed543',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        key: 'e40e743af2c94b0c916a8aa618fb4473',
+      },
+    });
+    return next.handle(request);
+  }
+}
