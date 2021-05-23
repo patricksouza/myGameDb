81ab7c01f98c2d14fc6535ad16ffe15244869f86
Patrick Miranda <patrickrsmiranda@gmail.com>
2021-05-23 19:34:06 -0400

    [015] Fixing some details on make requests to the API

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { APIResponse, Data } from 'app/models/data.model';
+import { HttpService } from 'app/services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public sort: string | undefined;
-  constructor() {}
+  public valuesList: Array<Data> = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private httpService: HttpService,
+    private activateRoute: ActivatedRoute
+  ) {}
+
+  //First method callend when rendering
+  ngOnInit(): void {
+    this.activateRoute.params.subscribe((params: Params) => {
+      this.searchGames('metacrit', params['game-search']);
+    });
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.httpService
+      .getValuesList(sort)
+      .subscribe((values: APIResponse<Data>) => {
+        this.valuesList = values.results;
+        console.log(this.valuesList);
+      });
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com'
+
 };
