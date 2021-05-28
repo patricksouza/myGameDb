b03014485edc48afaaf11a6c1455907daf803eda
vfxcvxc <xcvxcv>
2021-05-27 22:40:18 -0400

    [018] Working on homepage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,28 +9,49 @@ import { HttpService } from 'app/services/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public sort: string | undefined;
   public valuesList: Array<Data> = [];
+  private routeSub!: Subscription;
+  private valueSub!: Subscription;
 
   constructor(
     private httpService: HttpService,
+    private router: Router,
     private activateRoute: ActivatedRoute
   ) {}
 
   //First method callend when rendering
   ngOnInit(): void {
-    this.activateRoute.params.subscribe((params: Params) => {
-      this.searchGames('metacrit', params['game-search']);
+    this.routeSub = this.activateRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchValues('metacrit', params['game-search']);
+      } else {
+        this.searchValues('metacrit');
+      }
     });
   }
 
-  searchGames(sort: string, search?: string): void {
-    this.httpService
-      .getValuesList(sort)
+  searchValues(sort: string, search?: string): void {
+    this.valueSub = this.httpService
+      .getValuesList(sort, search)
       .subscribe((values: APIResponse<Data>) => {
         this.valuesList = values.results;
         console.log(this.valuesList);
       });
+  }
+
+  openValueDetails(id: string): void {
+    //Navigate to a page and pass an id
+    this.router.navigate(['details', id]);
+  }
+
+  ngOnDestroy(): void {
+    if (this.valueSub) {
+      this.valueSub.unsubscribe();
+    }
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
   }
 }
