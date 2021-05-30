be86581a30d71c20bebfe90f66718b954ddc9ed9
vfxcvxc <xcvxcv>
2021-05-29 23:17:53 -0400

    [022] Add an another type of search. By Genre

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -18,11 +18,11 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     request = request.clone({
       setHeaders: {
-        'x-rapidapi-key': '',
-        'x-rapidapi-host': '',
+        'x-rapidapi-key': 'eb4786c419mshe3372ce6646c58ep19f044jsn4878fa0ed543',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
       },
       setParams: {
-        key: '',
+        key: 'bb9686d2683746988c6fa1a6d583babe',
       },
     });
     return next.handle(request);
